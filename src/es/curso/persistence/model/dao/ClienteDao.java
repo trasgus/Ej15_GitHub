61950f168554db1a645b9c92966162e564871617
Usuario <Usuario@aula2-PC6>
2015-05-22 12:12:25 +0200

    UPDATE DAOS-> AgregarCliente

--- src/es/curso/persistence/model/dao/ClienteDao.java
+++ src/es/curso/persistence/model/dao/ClienteDao.java
@@ -1,6 +1,10 @@
 package es.curso.persistence.model.dao;
 
-public class ClienteDao {
+import es.curso.model.entity.Cliente;
+
+public interface ClienteDao {  //he quitado class y he puesto interface
 //Por cada entity q creemos (clases q tendrán reflejo en la base de datos), creamos un DAO
+	
+	public void create(Cliente cliente);//creo método abstracto  y me importo la clase Cliente
 	
 }
--- src/es/curso/persistence/model/dao/jdbc/ClienteDaoJdbc.java
+++ src/es/curso/persistence/model/dao/jdbc/ClienteDaoJdbc.java
@@ -1,5 +1,20 @@
 package es.curso.persistence.model.dao.jdbc;
 
-public class ClienteDaoJdbc {
+import es.curso.model.entity.Cliente;
+import es.curso.persistence.model.dao.ClienteDao;
+
+public class ClienteDaoJdbc implements ClienteDao{
+
+	
+	//Enlazo lenguaje java con sql
+	@Override
+	public void create(Cliente cliente) {
+		/* van las instrucciones para
+		  1. Conectar con la base de datos
+		  2. Preparar la sentencia -sql- para agregar
+		  3. Ejecutar la sentencia -sql- 
+		  4. Cerrar la conexión     */
+		
+	}
 
 }
