28036b9d568d726dcdb6009ffe613342e0ac5777
Usuario <Usuario@aula2-PC6>
2015-05-22 09:52:29 +0200

    UPDATE Clase Cliente o.k

--- src/es/curso/model/entity/Cliente.java
+++ src/es/curso/model/entity/Cliente.java
@@ -1,5 +1,106 @@
 package es.curso.model.entity;
 
 public class Cliente {
+	private int id;
+	private String nombres;
+	private String apellidos;
+	private String dni;
+	
+	public Cliente() {  //constructor vacio
+		super();
+	}
+
+	public Cliente(int id, String nombres, String apellidos, String dni) {   //constructor lleno
+		super();
+		this.id = id;
+		this.nombres = nombres;
+		this.apellidos = apellidos;
+		this.dni = dni;
+	}
+
+	public int getId() {    //getters and setters
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNombres() {
+		return nombres;
+	}
+
+	public void setNombres(String nombres) {
+		this.nombres = nombres;
+	}
+
+	public String getApellidos() {
+		return apellidos;
+	}
+
+	public void setApellidos(String apellidos) {
+		this.apellidos = apellidos;
+	}
+
+	public String getDni() {
+		return dni;
+	}
+
+	public void setDni(String dni) {
+		this.dni = dni;
+	}
+
+	@Override                 //generater toString
+	public String toString() {
+		return "Cliente [id=" + id + ", nombres=" + nombres + ", apellidos="
+				+ apellidos + ", dni=" + dni + "]";
+	}
+
+	@Override          // source generater hasCode() and equals()
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result
+				+ ((apellidos == null) ? 0 : apellidos.hashCode());
+		result = prime * result + ((dni == null) ? 0 : dni.hashCode());
+		result = prime * result + id;
+		result = prime * result + ((nombres == null) ? 0 : nombres.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Cliente other = (Cliente) obj;
+		if (apellidos == null) {
+			if (other.apellidos != null)
+				return false;
+		} else if (!apellidos.equals(other.apellidos))
+			return false;
+		if (dni == null) {
+			if (other.dni != null)
+				return false;
+		} else if (!dni.equals(other.dni))
+			return false;
+		if (id != other.id)
+			return false;
+		if (nombres == null) {
+			if (other.nombres != null)
+				return false;
+		} else if (!nombres.equals(other.nombres))
+			return false;
+		return true;
+	}
+	
+	
+	
+	
+	
+	
 
 }
